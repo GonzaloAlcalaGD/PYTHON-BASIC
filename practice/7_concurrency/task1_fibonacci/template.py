7d64f632557ed4ab3b7366db05761dc069423743
GonzaloAlcalaGD <galcala@griddynamics.com>
2022-07-01 14:45:53 -0500

    7_concurrency

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,31 +1,64 @@
+from multiprocessing import Pool
 import os
 from random import randint
+import timeit
+import csv
 
 
-OUTPUT_DIR = './output'
-RESULT_FILE = './output/result.csv'
-
+OUTPUT_DIR = '/Users/gonzo/Desktop/Python/PYTHON-BASIC/practice/7_concurrency/output/'
+RESULT_FILE = '/Users/gonzo/Desktop/Python/PYTHON-BASIC/practice/7_concurrency/output/result.csv'
+func1_store_results = []
 
 def fib(n: int):
     """Calculate a value in the Fibonacci sequence by ordinal number"""
-
     f0, f1 = 0, 1
     for _ in range(n-1):
         f0, f1 = f1, f0 + f1
     return f1
-
-
+    
 def func1(array: list):
-    pass
+    """
+    Calculates fibonacci numbers from a list of ordinal numbers
+    """
+    return fib(array)
+
+
+def write_func1(result_file: list, filenames: list):
+    """
+    Writes files from a list of ordinal numbers and their fibonacci numbers
+    """
+    for index in range(len(result_file)):
+        with open(OUTPUT_DIR+str(filenames[index])+'.txt', 'w') as f:
+            f.write(str(result_file[index]))
 
 
 def func2(result_file: str):
-    pass
+    with open(result_file, 'w') as w:
+        for filename in os.listdir(OUTPUT_DIR):
+            with open(OUTPUT_DIR+filename, 'r') as f:
+                writer = csv.writer(w)
+                writer.writerow([filename[:-4], f.read()])
+            
 
 
 if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
+   
+    # Create pool of workers
+    pool = Pool()
 
-    func1(array=[randint(1000, 100000) for _ in range(1000)])
+    # Start the timer
+    start = timeit.default_timer()
+    # Create a list of random numbers
+    array = [randint(1000, 100000) for _ in range(1000)]
+    # Call the function func1 with the list of random numbers into multiprocessing
+    f1 = pool.map(func1, array)
+    # Write files
+    write_func1(f1, array)
+    # Stop the timer
+    pool.close()
     func2(result_file=RESULT_FILE)
+    end = timeit.default_timer()
+    print(end-start)
+    # print(end-start)
--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,23 +1,36 @@
-API_KEY = "YOUR_KEY"
+from multiprocessing import Pool, cpu_count
+from urllib import response
+import requests
+import timeit 
+import urllib
+
+
+API_KEY = "gC2JBMjaYE3XE8NZIlK4rhQBeqQKYyx6baq4CtzI"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
-OUTPUT_IMAGES = './output'
+OUTPUT_IMAGES = '/Users/gonzo/Desktop/Python/PYTHON-BASIC/practice/7_concurrency/task2_apod_api/output'
 
 
-def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+def get_apod_metadata(metadata: list) -> list:
+    return requests.get(APOD_ENDPOINT, params= metadata).json()
 
 
-def download_apod_images(metadata: list):
-    pass
-
-
+def download_apod_images(metadata):
+    if metadata['media_type'] == 'image':
+        urllib.request.urlretrieve(metadata['url'], OUTPUT_IMAGES+'/'+metadata['date']+'.jpg')
+    else: 
+        pass
+    
+    
 def main():
-    metadata = get_apod_metadata(
-        start_date='2021-08-01',
-        end_date='2021-09-30',
-        api_key=API_KEY,
-    )
-    download_apod_images(metadata=metadata)
+    
+    metadata = get_apod_metadata({'start_date':'2021-08-01','end_date':'2021-09-30','api_key':API_KEY})
+    print('Starting multiprocessing with {}'.format(cpu_count()))
+    pool = Pool()  # Create pool of workers
+    start = timeit.default_timer()
+    download = pool.map(download_apod_images, metadata)
+    end = timeit.default_timer()
+    pool.close()
+    print('Finished multiprocessing in {}'.format(end-start))
 
 
 if __name__ == '__main__':
